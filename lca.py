8dacab7de3879f913d86afaa66c484a1e8b8ddf3
Darragh Hurley <hurleyd4@tcd.ie>
2018-10-16 12:48:10 +0100

    Further improved my lca.py solution to work with a path from root to each node. Will begin to compare all paths of both nodes soon. Also fixed some errors within tests that were not returning correct values

--- lca.py
+++ lca.py
@@ -1,7 +1,6 @@
 # Python Program for Lowest Common Ancestor in a Binary Tree
 # O(n) solution to find LCS of two given values n1 and n2
 
-# A binary tree node
 class Graph(object):
 
     def __init__(self, graph_dict=None):
@@ -17,40 +16,49 @@ class Graph(object):
 # exists otherwise false
     def findPath(self, root, end, path = None):
 
-        try:
+        #try:
             # Baes Case
             if path == None:
                 path = []
-
             graph = self.__graph_dict
-            path = path + [start_vertex]
+            path = path + [root]
+            if root == end:
+                return path
+            if root not in graph:
+                return None
+            for vertex in graph[root]:
+                if vertex not in path:
+                    extended_path = self.findPath(vertex,
+                                                   end,
+                                                   path)
+                    if extended_path:
+                        return extended_path
+            return None
 
-            # Check if k is found in left or right sub-tree
-            if ((root.left != None and findPath(root.left, path, k)) or
-                    (root.right!= None and findPath(root.right, path, k))):
-                return True
-
-                # If not present in subtree rooted with root, remove
-            # root from path and return False
-
-            path.pop()
-            return False
-
-        except AttributeError as error:     #prevents error being thrown for invalid input
-            print "Invalid key input"
+        #except AttributeError as error:     #prevents error being thrown for invalid input
+        #    print "Invalid key input"
 
     # Returns LCA if node n1 , n2 are present in the given
     # binary tre otherwise return -1
     def findLCA(self, root, n1, n2):
 
-        # To store paths to n1 and n2 fromthe root
+        # To store paths to n1 and n2 from the root
         path1 = []
         path2 = []
-
         # Find paths from root to n1 and root to n2.
         # If either n1 or n2 is not present , return -1
-        path1 = self.find_path(root,n1)
-        path2 = self.find_path(root, n2)
+        path1 = self.findPath(root,n1)
+        path2 = self.findPath(root,n2)
+        print("Path1: ")
+        print(path1)
+        print("Path2: ")
+        print(path2)
+
+        if path1 == None:
+            return -1;
+
+        if path2 == None:
+            return -1;
 
         # Compare the paths to get the first different value
         i = 0
--- lca_test.py
+++ lca_test.py
@@ -5,54 +5,50 @@ class testLCA(unittest.TestCase):
 
     def test_null_tree(self):                           #testing both functions for null binary search trees
         g = {}
-        self.assertEqual( g.findLCA(None,"a","b"), -1)        #as there is no root the function should return -1
-        self.assertEqual( g.findPath("a","b"), False)    #as there is no root the function should return False
+        graph = Graph(g)
+        self.assertEqual(graph.findLCA(None,"a","b"), -1)        #as there is no root the function should return -1
+        self.assertEqual(graph.findPath("a","b"), None)    #as there is no root the function should return False
 
     def test_root_only(self):                           #testing for a tree with only one node
         g = {"a"}                                  #setting root equal to nodewith key 1
+        graph = Graph(g)
         path =[]                                        #empty path to pass into findPath function
-        self.assertEqual( g.findPath("a","a"), True)  #confirming that path can be found when only the root is available
-        self.assertEqual( g.findLCA("a","a","a"), "a")         #confirming that the path that was found gives the correct lca for only root
+        self.assertEqual(graph.findPath("a","a"), ["a"])  #confirming that path can be found when only the root is available
+        self.assertEqual(graph.findLCA("a","a","a"), "a")         #confirming that the path that was found gives the correct lca for only root
 
     def test_invalid_key(self):                         #testing for an invalid node entry
-        g = { "a" : ["b","d"],
-              "b" : ["c"],
-              "c" : ["b", "c", "d", "e"],
-              "d" : ["b", "e", "f"],
-              "e" : ["c"],
-              "f" : ["c"]
+        g = { "a" : ["b","c"],
+              "b" : ["d"],
+              "c" : ["d", "e"],
             }
+        graph = Graph(g)
         path =[]                                        #
-        self.assertEqual(g.findPath("a",8), False)      #the key 8 is invalid, should return False and print "Invalid key input"
-        self.assertEqual(g.findLCA("a",8),-1)           #again 8 is invalid, should return -1
+        self.assertEqual(graph.findPath("a",8), None)      #the key 8 is invalid, should return None and print "Invalid key input"
+        self.assertEqual(graph.findLCA("a",8, 9),-1)           #again 8 is invalid, should return -1
 
     def test_invalid_type(self):                        #testing for an invald type entry
-        g = { "a" : ["b","d"],
-              "b" : ["c"],
-              "c" : ["b", "c", "d", "e"],
-              "d" : ["b", "e", "f"],
-              "e" : ["c"],
-              "f" : ["c"]
+        g = { "a" : ["b","c"],
+              "b" : ["d"],
+              "c" : ["d", "e"],
             }
+        graph = Graph(g)
         path =[]                                        #
-        self.assertEqual(g.findLCA("a",path,"text"),-1)  #a string should return -1 when passed in as a key
-        self.assertEqual(g.findLCA(path,-10,"text"),-1)   #path is not a valid root and again text the incorrect input for a key
+        self.assertEqual(graph.findLCA("a",path,"text"),-1)  #a string should return -1 when passed in as a key
+        #self.assertEqual(graph.findLCA(path,-10,"text"),-1)   #path is not a valid root and again text the incorrect input for a key
 
-    def test_successful_lca(self):          #           BST Example
-        g = { "a" : ["b","d"],
-              "b" : ["c"],
-              "c" : ["b", "c", "d", "e"],
-              "d" : ["b", "e", "f"],
-              "e" : ["c"],
-              "f" : ["c"]
+    def test_successful_basic_lca(self):          #           BST Example
+        g = { "a" : ["b","c"],
+              "b" : ["d"],
+              "c" : ["d", "e"],
             }
-        self.assertEqual(g.findLCA(root,1,5),4)#these tests are to show that LCA works as intended
-        self.assertEqual(g.findLCA(root,7,4),6)
-        self.assertEqual(g.findLCA(root,8,3),3)
-        self.assertEqual(g.findLCA(root,1,8),6)
-        self.assertEqual(g.findLCA(root,8,8),8) # testing to see if the lca of a node is itself
+        graph = Graph(g)
+        self.assertEqual(graph.findLCA("a","b","e"),"a")#these tests are to show that LCA works as intended
+        self.assertEqual(graph.findLCA("a","d","e"),"a")
+        self.assertEqual(graph.findLCA("a","a","d"),"a")
+        self.assertEqual(graph.findLCA("a","c","d"),"a")
+        self.assertEqual(graph.findLCA("a","b","b"),"b") # testing to see if the lca of a node is itself
 
-    #need to begin testing for directed acyclic graph
+    #will add a test for a more complicated DAG
 
 if __name__ == '__main__':
     unittest.main()
